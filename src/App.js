494688e856d9a3edc51eb255ac6a30be6a353384
Brendan <brendanmoratz@hotmail.com>
2020-08-12 14:23:09 +0200

    Basic styling and add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import SimpleLineChart from './SimpleLineChart'
+import Table from './Table'
+import SimpleBarChart from './SimpleBarChart'
+import styled from 'styled-components/macro'
+
+const StyledMain = styled.main`
+  max-width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'></header>
-      <main></main>
+      <StyledMain>
+        <SimpleLineChart />
+        <SimpleBarChart />
+        <Table />
+      </StyledMain>
       <footer></footer>
     </div>
   )
